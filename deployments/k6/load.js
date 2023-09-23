f9dc88f8e752b5ed8df66313c9a35ee6941d3ad8
Sonja Chèvre <sonja+github@tyk.io>
2023-09-23 16:59:24 +0200

    update scenario

--- deployments/k6/load.js
+++ deployments/k6/load.js
@@ -1,8 +1,7 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
-var testDuraction = '1m';
+var testDuraction = '15m';
 
 const params = {
   headers: { 'Content-Type': 'application/json', 'x-tyk-authorization': '28d220fd77974a4facfb07dc1e49c2aa', 'Response-Type': 'application/json' },
@@ -16,22 +15,49 @@ const params = {
 
 export function setup() {
 
-  var alias1 = "user102";
-  var alias2 = "user104";
+  var alias1 = "mobile-app";
+  var alias2 = "website";
+  var alias3 = "screen-app";
+
   var users = [];
 
   var data = '{"alias": "' + alias1 
       + '", "expires": -1, "access_rights": { '
-      + '"1": { "api_id": "1","api_name": "flight information","versions": ["Default"]}'
-      + '"2": { "api_id": "2","api_name": "baggage tracking","versions": ["Default"]}'
-      + '"3": { "api_id": "3","api_name": "parking reservation","versions": ["Default"]}'
+      + ' "1": { "api_id": "1", "api_name": "flight information","versions": ["Default"]},'
+      + ' "2": { "api_id": "2", "api_name": "baggage tracking","versions": ["Default"]},'
+      + ' "3": { "api_id": "3", "api_name": "parking reservation","versions": ["Default"]}'
     + '}}' ;
-
 
   let res = http.post("http://host.docker.internal:8080/tyk/keys/create", data, params);
 
   var authKey1 = res.json().key;
   users[0] = [alias1, authKey1];
+
+  var data2 = '{"alias": "' + alias2 
+      + '", "expires": -1, "access_rights": { '
+      + ' "1": { "api_id": "1", "api_name": "flight information","versions": ["Default"]},'
+      + ' "2": { "api_id": "2", "api_name": "baggage tracking","versions": ["Default"]},'
+      + ' "3": { "api_id": "3", "api_name": "parking reservation","versions": ["Default"]}'
+    + '}}' ;
+
+  res = http.post("http://host.docker.internal:8080/tyk/keys/create", data2, params);
+
+  var authKey2 = res.json().key;
+  users[1] = [alias2, authKey2];
+
+  var data3 = '{"alias": "' + alias3 
+      + '", "expires": -1, "access_rights": { '
+      + ' "1": { "api_id": "1", "api_name": "flight information","versions": ["Default"]},'
+      + ' "2": { "api_id": "2", "api_name": "baggage tracking","versions": ["Default"]},'
+      + ' "3": { "api_id": "3", "api_name": "parking reservation","versions": ["Default"]}'
+    + '}}' ;
+
+  res = http.post("http://host.docker.internal:8080/tyk/keys/create", data3, params);
+
+  var authKey3 = res.json().key;
+  users[2] = [alias3, authKey3];
+
+
 
   return { users };
 }
@@ -43,18 +69,36 @@ export const options = {
     user_102: {
       executor: 'constant-arrival-rate',
       exec: 'user_102',
+      rate: 2,
+      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+      duration: testDuraction,
+      preAllocatedVUs: 1, // how large the initial pool of VUs would be
+      maxVUs: 5, // if the preAllocatedVUs are not enough, we can initialize more
+    },
+    user_104: {
+      executor: 'constant-arrival-rate',
+      exec: 'user_104',
       rate: 3,
       timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
       duration: testDuraction,
       preAllocatedVUs: 1, // how large the initial pool of VUs would be
-      maxVUs: 1, // if the preAllocatedVUs are not enough, we can initialize more
+      maxVUs: 5, // if the preAllocatedVUs are not enough, we can initialize more
+    },
+    user_105: {
+      executor: 'constant-arrival-rate',
+      exec: 'user_105',
+      rate: 25,
+      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+      duration: testDuraction,
+      preAllocatedVUs: 1, // how large the initial pool of VUs would be
+      maxVUs: 5, // if the preAllocatedVUs are not enough, we can initialize more
     }
 
   }
 }
 
 export default function (data) {
-  // console.log("default function: " + JSON.stringify(data.authKey));
+  //console.log("default function: " + JSON.stringify(data.authKey));
 }
 
 export function user_102(data) {
@@ -65,6 +109,31 @@ export function user_102(data) {
 
   http.get('http://host.docker.internal:8080/baggage-tracking/get', params);
   http.get('http://host.docker.internal:8080/flight-information/get', params);
-  http.get('http://host.docker.internal:8080//parking-reservation/get', params);
+  http.get('http://host.docker.internal:8080/parking-reservation/get', params);
+
+}
+
+export function user_104(data) {
+
+  const params = {
+    headers: { 'authorization': data.users[1][1] }
+  };
+
+  http.get('http://host.docker.internal:8080/baggage-tracking/get', params);
+  http.get('http://host.docker.internal:8080/flight-information/get', params);
+  http.get('http://host.docker.internal:8080/parking-reservation/get', params);
+
+}
+
+
+export function user_105(data) {
+
+  const params = {
+    headers: { 'authorization': data.users[2][1] }
+  };
+
+  http.get('http://host.docker.internal:8080/baggage-tracking/status/500', params);
+  http.get('http://host.docker.internal:8080/flight-information/status/500', params);
+  http.get('http://host.docker.internal:8080/parking-reservation/get', params);
 
 }
